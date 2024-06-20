106fc6029f7b0fa5240d92c46a13c4461f5b199b
nh-nayeem <git@github.com:nh-nayeem>
2024-06-20 15:55:32 +0600

    modified:   src/app/page.js

--- src/app/page.js
+++ src/app/page.js
@@ -12,7 +12,7 @@ export default function Home() {
           Home
         </button>
         <button className="bg-blue-400 hover:bg-blue-700 text-white font-bold py-1 px-2 rounded">
-          Family Three
+          Family Tree
         </button>
         <button className="bg-blue-400 hover:bg-blue-700 text-white font-bold py-1 px-2 rounded">
           Contact
