0796c7bc1efe4834c60f97aaa767088d5112fa73
nh-nayeem <git@github.com:nh-nayeem>
2024-06-23 20:24:19 +0600

    modified:   src/app/familytree/page.js 	modified:   src/app/page.js

--- src/app/familytree/page.js
+++ src/app/familytree/page.js
@@ -8,15 +8,10 @@ export default function Family() {
     <main className="flex min-h-screen px-3">
       <nav className="fixed top-0 z-50 mb-25 grid text-center bg-white bg-opacity-50 px-6 py-3 shadow-sm lg:max-w-7xl lg:w-full lg:mb-0 lg:grid-cols-8 lg:text-center">
           <Link href="/"> <h1 className="flex text-2xl font-semibold">Molla Bari </h1></Link>
-          <h1></h1><h1></h1>
-          <Link href="/">Home</Link>
-          <Link href="/familytree">Family Tree</Link>
-          <Link href="/contact">Contact</Link>
-          <h1></h1><h1></h1>
+          <Link href="/familytree">Molla Family</Link>
       </nav>  
-      <div className="flex grid rows-2 w-full">
+      <div className="flex grid rows-1 w-full">
         <div className='h-auto py-20'>
-          <h1 className='px-10 font-semibold'>Molla Family</h1>
           <FamilyTree/>
         </div>
       </div>
--- src/app/page.js
+++ src/app/page.js
@@ -5,14 +5,10 @@ import Link from "next/link";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-31">
-      <nav className="fixed top-0 z-50 mb-25 grid text-top bg-white bg-opacity-50 px-6 py-3 shadow-sm lg:max-w-7xl lg:w-full lg:mb-0 lg:grid-cols-8 lg:text-center">
+      <nav className="fixed top-0 z-50 mb-25 grid text-center bg-white bg-opacity-50 px-6 py-3 shadow-sm lg:max-w-7xl lg:w-full lg:mb-0 lg:grid-cols-8 lg:text-center">
           <Link href="/"> <h1 className="flex text-2xl font-semibold">Molla Bari </h1></Link>
-          <h1></h1><h1></h1>
-          <Link href="/">Home</Link>
-          <Link href="/familytree">Family Tree</Link>
-          <Link href="/contact">Contact</Link>
-          <h1></h1><h1></h1>
-      </nav>
+          <Link href="/familytree">Molla Family</Link>
+      </nav> 
       <div className="flex top-1 grid px-6 py-20 place-items-center font-bold">
         <div className="border border-gray-300 rounded-lg p-2">
           <h1 className="text-3xl font-semibold text-center">Diachara Molla Bari</h1>
