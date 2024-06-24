fccee9103bf470d869c12980a8d8a9232c30e212
nh-nayeem <git@github.com:nh-nayeem>
2024-06-24 23:38:28 +0600

    modified:   src/app/familytree/page.js 	modified:   src/app/familytree/persons.json 	modified:   src/app/page.js

--- src/app/familytree/page.js
+++ src/app/familytree/page.js
@@ -7,8 +7,8 @@ export default function Family() {
   return (
     <main className="flex min-h-screen px-3">
       <nav className="fixed top-0 z-50 mb-25 grid text-center bg-white bg-opacity-50 px-6 py-3 shadow-sm lg:max-w-7xl lg:w-full lg:mb-0 lg:grid-cols-8 lg:text-center">
-          <Link href="/"> <h1 className="flex text-2xl font-semibold">Molla Bari </h1></Link>
-          <Link href="/familytree">Molla Family</Link>
+          <Link href="/"> <h1 className="flex text-2xl font-semibold">মোল্লা বাড়ী </h1></Link>
+          <Link href="/familytree">মোল্লা পরিবার </Link>
       </nav>  
       <div className="flex grid rows-1 w-full">
         <div className='h-auto py-20'>
--- src/app/page.js
+++ src/app/page.js
@@ -6,12 +6,12 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-31">
       <nav className="fixed top-0 z-50 mb-25 grid text-center bg-white bg-opacity-50 px-6 py-3 shadow-sm lg:max-w-7xl lg:w-full lg:mb-0 lg:grid-cols-8 lg:text-center">
-          <Link href="/"> <h1 className="flex text-2xl font-semibold">Molla Bari </h1></Link>
-          <Link href="/familytree">Molla Family</Link>
+          <Link href="/"> <h1 className="flex text-2xl font-semibold">মোল্লা বাড়ী </h1></Link>
+        <Link href="/familytree">মোল্লা পরিবার</Link>
       </nav> 
       <div className="flex top-1 grid px-6 py-20 place-items-center font-bold">
         <div className="border border-gray-300 rounded-lg p-2">
-          <h1 className="text-3xl font-semibold text-center">Diachara Molla Bari</h1>
+          <h1 className="text-3xl font-semibold text-center">দায়চারা মোল্লা বাড়ী</h1>
         </div>
         <div className="flex grid py-10 item-left lg:grid-cols-2 gap-10">
           <div className="border border-gray-300 rounded-lg p-2">
@@ -22,10 +22,9 @@ export default function Home() {
               height={500}
             />
             <p>
-            Dighi: <br/>
-            The signature of the Diachara Molla bari is its large Dighi.<br/>
-            In 1952 the Dighi was excavated by hiring some labour at <br/>
-            a cost of 8000BDT.
+            দীঘি: <br/>
+            মোল্লা বাড়ীতে আসলেই এর দৃষ্টিনন্দন দীঘি চোখে পড়ে। <br/>
+            এ দীঘি ১৯৫২ সালে ৮০০০ টাকা মজুরীর বিনিময়ে খনন করা হয়ছিলো। <br/>
             </p>
           </div>
           <div className="border border-gray-300 rounded-lg p-2">
@@ -36,9 +35,9 @@ export default function Home() {
               height={500}
             />
             <p>
-            Kachari Ghor: <br/>
-            This is the very recent version of the Kachari ghor. <br/>
-            It is used for praying namaj and also teaching Quran.
+            কাছারি ঘর: <br/>
+            মোল্লা বাড়ীর পুরোনো কাছারি ঘর অল্প কিছুদিন আগে পাকা করা হয়। <br/>
+            অনেক কাল আগে থেকেই নামাজের জায়গা আর মক্তব হিসেবে <br/> ব্যবহৃত হয়ে আসছে। <br/>
             </p>
           </div>
         </div>
